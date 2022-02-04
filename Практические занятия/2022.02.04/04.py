f8db339d036031137f660bdf9b094879ee544473
NikitaNik_of <ncayka@gmail.com>
2022-02-04 11:10:27 +0300

    Практика

--- Практические занятия/2022.02.04/04.py
+++ Практические занятия/2022.02.04/04.py
@@ -1,5 +1,9 @@
 n = int(input('n = '))
+
 ltt = []
 for i in range(1, n + 1):
     ltt.append([i for i in range(1, i + 1)])
 print(ltt)
+
+ltt_ = [[i for i in range(1, j + 1)] for j in range(1, n + 1)]
+print(ltt_)
