f6a8fe7b883320c5e6d7ce015dda627d39c8bbd7
NikitaNik_of <ncayka@gmail.com>
2022-04-13 09:39:22 +0300

    + занятие

--- Технологии программирования/Словари/21-22.py
+++ Технологии программирования/Словари/21-22.py
@@ -10,14 +10,14 @@ import json
 # print(table)
 
 # Формирование таблицы через file
-with open("Технологии программирования\\Словари\\table.txt", 'r2+') as f:
+with open("Технологии программирования\\Словари\\table.txt", 'r+') as f:
     n = int(f.readline())
     m = int(f.readline())
     c = ['C' + str(_ + 1) for _ in range(n)]
     v = ['V' + str(_ + 1) for _ in range(m)]
     c_data = [ dict( zip(v, list(map(float, f.readline().split())))) for _ in range(n)]
     table = dict(zip(c, c_data))
-    print(json.dump(table, sort_keys=True, indent=4))
+    print(json.dumps(table, sort_keys=True, indent=4))
 
 # Вопрос
 q = int(input('V1 > ? | >> '))
